dd4ec8ef1dd4b319343535662545790baff8bed4
Rosnye <davd326at@gmail.com>
2024-03-11 20:32:37 -0600

    Added responsive design

--- src/components/AboutMe/index.jsx
+++ src/components/AboutMe/index.jsx
@@ -4,7 +4,7 @@ const AboutMe = () => {
     return (
         <div className="rounded-xl flex flex-col my-6 p-6">
             <div className="flex p-4 justify-evenly border-gray-300 rounded-xl bg-gray-500 border-b border-w-">
-                <div className="h-32 w-32 bg-white flex items-center justify-center border border-white rounded-xl"><img src="./images/Foto_perfil.jpg" 
+                <div className="sm:block hidden h-32 w-32 bg-white items-center justify-center border border-white rounded-xl"><img src="./images/Foto_perfil.jpg" 
                     alt="Foto perfil" className="rounded-xl"/></div>
                 <div className="flex flex-col px-6">
                     <h2 className="text-white font-bold text-xl mb-4">
@@ -26,16 +26,16 @@ const AboutMe = () => {
             </div>
             <div className="border-b border-gray-300 w-full p-6 flex flex-col bg-gray-500 rounded-xl mt-2">
                 <h2 className="text-white text-xl">Tech Skills:</h2>
-                <div className="h-14 flex justify-evenly items-center mt-8 text-white">
-                    <div className="h-10 mr-4 flex items-center"><img className="h-12 mr-4 bg-slate-800 rounded p-2" src="./assets/html-5.svg" alt="html icon"/> HTML</div>
-                    <div className="h-10 mr-4 flex items-center"><img className="h-12 mr-4 bg-slate-800 rounded p-2" src="./assets/css-3.svg" alt="css icon"/>CSS</div>
-                    <div className="h-10 mr-4 flex items-center"><div className="h-12 mr-4 bg-slate-800 rounded p-2">
+                <div className="flex flex-col sm:flex-row justify-evenly sm:items-center mt-8 text-white">
+                    <div className="h-10 my-2 mr-4 flex items-center"><img className="h-12 mr-4 bg-slate-800 rounded p-2" src="./assets/html-5.svg" alt="html icon"/> HTML</div>
+                    <div className="h-10 my-2 mr-4 flex items-center"><img className="h-12 mr-4 bg-slate-800 rounded p-2" src="./assets/css-3.svg" alt="css icon"/>CSS</div>
+                    <div className="h-10 my-2 mr-4 flex items-center"><div className="min-w-12 h-12 mr-4 bg-slate-800 rounded p-2">
                         <img className="animate-spin-slow" src="./assets/react.svg" alt="react-logo" /></div> REACT JS</div>
-                    <div className="h-10 mr-4 flex items-center"><img className="h-12 mr-4 bg-slate-800 rounded p-2" 
+                    <div className="h-10 my-2 mr-4 flex items-center"><img className="h-12 mr-4 bg-slate-800 rounded p-2" 
                         src="./assets/tailwind-icon.svg" alt="tailwind icon" /> TAILWIND</div>
-                    <div className="h-10 mr-4 flex items-center"><img className="h-12 mr-4 bg-slate-800 rounded p-2" 
+                    <div className="h-10 my-2 mr-4 flex items-center"><img className="h-12 mr-4 bg-slate-800 rounded p-2" 
                         src="./assets/nodejs-icon.svg" alt="nodejs icon" /> NODE JS</div>
-                    <div className="h-10 mr-4 flex items-center"><img className="h-12 mr-4 bg-slate-800 rounded p-2" 
+                    <div className="h-10 my-2 mr-4 flex items-center"><img className="h-12 mr-4 bg-slate-800 rounded p-2" 
                         src="./assets/javascript-icon.svg" alt="javascript icon" /> JAVASCRIPT</div>
                 </div>
             </div>
--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -7,9 +7,9 @@ const Nav = () => {
         <nav className="flex items-center justify-between border-b border-gray-300 w-full p-4">
             <div className="flex items-center ml-8">
                 <img src="./images/david-icon.jpeg" alt="Logo" className="rounded h-10 w-10 mr-4"/>
-                <button className="text-white text-2xl hover:underline">David Fletes Dev</button>
+                <button className="text-white text-2xl hover:underline">David Dev</button>
             </div>
-            <div className="flex mr-6">
+            <div className="hidden mr-6">
                 <button className="bg-gray-900 hover:bg-gray-600 text-white font-medium py-2 px-4 rounded">Home</button>
                 <button className="bg-gray-900 hover:bg-gray-600 text-white font-medium py-2 px-4 ml-4 rounded">About Me</button>
                 <button className="bg-gray-900 hover:bg-gray-600 text-white font-medium py-2 px-4 ml-4 rounded">Proyects</button>
