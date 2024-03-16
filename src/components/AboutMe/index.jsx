ac6916bd715461e3db7a5cdf85585e40759dc667
Dave <davd326at@gmail.com>
2024-03-16 01:23:04 -0600

    Added hover animations to buttons linkedin github

--- src/components/AboutMe/index.jsx
+++ src/components/AboutMe/index.jsx
@@ -3,11 +3,13 @@ import React from "react";
 const Info = () => {
     return (
         <div className="flex p-4 justify-evenly ">
-                <div className="sm:block hidden w-48 bg-white items-center justify-center border border-white rounded-xl"><img src="./images/Foto_perfil.jpg" 
-                    alt="Foto perfil" className="rounded-xl"/></div>
+                <div className="sm:block hidden w-48 bg-white items-center justify-center border border-white rounded-xl">
+                    <img src="./images/Foto_perfil.jpg" alt="Foto perfil" className="rounded-xl"/></div>
+
                 <div className="flex flex-col px-6">
                     <h2 className="text-white font-bold text-xl xl:text-4xl mb-4">
-                        Hey i'm David <br/> <h2 className="bg-gradient-to-br from-sky-400 to-indigo-200 inline-block text-transparent bg-clip-text">Front End React Developer </h2> 
+                        Hey i'm David <br/> <h2 className="bg-gradient-to-br from-sky-400 to-indigo-200 inline-block 
+                        text-transparent bg-clip-text">Front End React Developer </h2> 
                     </h2>
                     <p className="text-white font-normal text-base lg:text-2xl">
                         I'm David Fletes. Front End React Developer <br/>
@@ -17,9 +19,11 @@ const Info = () => {
 
                 <div className="flex flex-col justify-evenly">
                     <button><a href="https://www.linkedin.com/in/david-fletes-esparza-729018228" className="flex items-center pb-2">
-                        <img className="h-12 mr-4 bg-slate-400 rounded p-2" src="./assets/linkedin-icon.svg" alt="email icon" /></a></button>
+                        <img className="h-12 mr-4 bg-slate-400 transition duration-300 hover:bg-slate-200 hover:scale-110 rounded p-2" 
+                            src="./assets/linkedin-icon.svg" alt="email icon" /></a></button>
                     <button><a href="https://github.com/Rosnye" className="flex items-center pb-2">
-                        <img className="h-12 mr-4 bg-slate-400 rounded p-2" src="./assets/github-icon.svg" alt="email icon" /></a></button>
+                        <img className="h-12 mr-4 bg-slate-400 transition duration-300 hover:bg-slate-200 hover:scale-110 rounded p-2" 
+                            src="./assets/github-icon.svg" alt="email icon" /></a></button>
                 </div>
             </div>
     );
@@ -30,8 +34,10 @@ const Skills = () => {
         <div className="w-full p-6 flex flex-col mt-2">
                 <h2 className="text-white text-2xl xl:text-4xl">Tech Skills:</h2>
                 <div className="flex flex-col lg:flex-row justify-evenly lg:items-center mt-8 text-white lg:text-xl">
-                    <div className="h-10 my-2 mr-4 flex items-center"><img className="h-12 mr-4 bg-slate-800 rounded p-2" src="./assets/html-5.svg" alt="html icon"/> HTML</div>
-                    <div className="h-10 my-2 mr-4 flex items-center"><img className="h-12 mr-4 bg-slate-800 rounded p-2" src="./assets/css-3.svg" alt="css icon"/>CSS</div>
+                    <div className="h-10 my-2 mr-4 flex items-center"><img className="h-12 mr-4 bg-slate-800 rounded p-2" 
+                        src="./assets/html-5.svg" alt="html icon"/> HTML</div>
+                    <div className="h-10 my-2 mr-4 flex items-center"><img className="h-12 mr-4 bg-slate-800 rounded p-2" 
+                        src="./assets/css-3.svg" alt="css icon"/>CSS</div>
                     <div className="h-10 my-2 mr-4 flex items-center"><div className="w-12 h-12 mr-4 bg-slate-800 rounded p-2">
                         <img className="animate-spin-slow" src="./assets/react.svg" alt="react-logo" /></div> REACT JS</div>
                     <div className="h-10 my-2 mr-4 flex items-center"><img className="h-12 mr-4 bg-slate-800 rounded p-2" 
@@ -50,15 +56,25 @@ const Contact = () => {
         <div className="w-full p-6 flex flex-col mt-2">
                 <h2 className="text-white text-lg lg:text-4xl">Contact Info:</h2>
                 <div className="justify-evenly items-center text-white pt-6 lg:text-xl">
-                    <div className="flex items-center pb-2"><img className="h-12 mr-4 bg-slate-400 rounded p-2" src="./assets/email-icon.svg" 
+                    <div className="flex items-center pb-2"><img className="h-12 mr-4 bg-slate-400 rounded p-2" 
+                        src="./assets/email-icon.svg" 
                         alt="email icon" /> Email: david.fletes.dev@gmail.com</div>
-                    <div className="flex items-center pb-2"><img className="h-12 mr-4 bg-slate-400 rounded p-2" src="./assets/telephone-icon.svg" 
+
+                    <div className="flex items-center pb-2"><img className="h-12 mr-4 bg-slate-400 rounded p-2" 
+                        src="./assets/telephone-icon.svg" 
                         alt="email icon" /> Telephone: +52 3314961922</div>
-                    <div className="flex items-center pb-2"><img className="h-12 mr-4 bg-slate-400 rounded p-2" src="./assets/linkedin-icon.svg" 
-                        alt="email icon" /><a className="hover:underline hover:underline-offset-2" href="https://www.linkedin.com/in/david-fletes-esparza-729018228">
+
+                    <div className="flex items-center pb-2"><button><a href="https://www.linkedin.com/in/david-fletes-esparza-729018228" 
+                        className="flex items-center">
+                        <img className="h-12 mr-4 bg-slate-400 transition duration-300 hover:bg-slate-200 hover:scale-110 
+                            rounded p-2" src="./assets/linkedin-icon.svg" alt="email icon" /></a></button><a className="hover:underline 
+                            hover:underline-offset-2" href="https://www.linkedin.com/in/david-fletes-esparza-729018228">
                             LinkedIn: linkedin.com/david-fletes</a></div>
-                    <div className="flex items-center pb-2"><img className="h-12 mr-4 bg-slate-400 rounded p-2" src="./assets/github-icon.svg" 
-                        alt="email icon" /><a className="hover:underline hover:underline-offset-2" href="https://www.github.com/Rosnye">Github: github.com/Rosnye </a></div>
+
+                    <div className="flex items-center pb-2"><button><a href="https://github.com/Rosnye" className="flex items-center">
+                        <img className="h-12 mr-4 bg-slate-400 transition duration-300 hover:bg-slate-200 hover:scale-110 rounded p-2" 
+                            src="./assets/github-icon.svg" alt="email icon" /></a></button><a className="hover:underline 
+                            hover:underline-offset-2" href="https://www.github.com/Rosnye">Github: github.com/Rosnye </a></div>
 
                 </div>
             </div>
@@ -69,7 +85,7 @@ const AboutMe = () => {
     return (
         <div className="rounded-xl flex flex-col my-6 p-6">
             <Info/>
-
+            
             <Skills/>
             
             <Contact/>
