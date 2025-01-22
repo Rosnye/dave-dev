1814d0bc1d05dec9d3f5ff99d7744968eaf9f46f
Dave <davd326at@gmail.com>
2025-01-22 11:51:34 -0600

    hotfix css commented

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import Card from "./components/Card"
 import Nav from "./components/Nav"
-import React, { useState } from "react"
 
 function App() {
 
-  let color = ""
-  const [theme,setTheme] = useState(true)
-  if (theme){
-    color="dark"
-  }
-  else{
-    color="white"
-  }
+  {/*let color = ""
+    const [theme,setTheme] = useState(true)
+    if (theme){
+      color="dark"
+    }
+    else{
+      color="white"
+    }  */}
+  
 
   return (
     <div className={`bg-slate-800 min-w-dvw min-h-dvh flex flex-col items-center`}>
