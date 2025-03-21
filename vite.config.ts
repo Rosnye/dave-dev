f00a40c6014eae0a8f042fba1a987496754c0828
David <lamaso02david@gmail.com>
2025-03-21 01:09:28 -0500

    Change config to deploy on vercel

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,19 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/dave-dev/",
+  build: {
+    outDir: 'dist', // Asegúrate de que la salida sea la carpeta 'dist'
+  },
+  base: '/',
 })
+
+/*import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist', // Asegúrate de que la salida sea la carpeta 'dist'
+  },
+  base: '/', // Asegúrate de que la base sea '/'
+});*/
