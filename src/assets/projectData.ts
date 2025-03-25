960f37aa8b852ce8c8cfa0fd0e743196c341826f
David <lamaso02david@gmail.com>
2025-03-25 18:38:00 -0500

    Add description to data

--- src/assets/projectData.ts
+++ src/assets/projectData.ts
@@ -1,7 +1,7 @@
 export const projectData = {
     skills: [
-      { name: 'OMDB API', image: 'OMDB_API.png', dir: 'https://rosnye.github.io/Movie-App/' },
-      { name: 'Pokemon Rest API', image: 'PokeApi.png', dir: 'https://rosnye.github.io/Pokeapi/' },
-      { name: 'Dinamic Forms', image: 'TeamOrg.png', dir: 'https://rosnye.github.io/org/' },
+      { name: 'OMDB API', image: 'OMDB_API.png', dir: 'https://rosnye.github.io/Movie-App/', description: 'This is a simple movie app that uses the OMDB API to search for movies and display their information.' },
+      { name: 'Pokemon Rest API', image: 'PokeApi.png', dir: 'https://rosnye.github.io/Pokeapi/', description: 'This is a simple app that uses the PokeAPI to search for Pokemon and display their information.' },
+      { name: 'Dinamic Forms', image: 'TeamOrg.png', dir: 'https://rosnye.github.io/org/', description: 'This is a simple app that uses the TeamOrg API to search for teams and display their information.' },
     ]
   }; 
